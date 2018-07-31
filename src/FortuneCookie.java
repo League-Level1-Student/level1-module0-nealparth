d918b5f9f5450305e1db872ce01ac2c9b1593f0e
MairaCCB <league@WTS14-4.attlocal.net>
2018-07-30 18:25:24 -0700

    FortuneCookie,JackInTheBox(IP)

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,14 +1,47 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class FortuneCookie {
-public static void main(String[] args) {
-	FortuneCookie fc = new FortuneCookie();
-	fc.showButton();
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-}
+public class FortuneCookie implements ActionListener {
+	public static void main(String[] args) {
+		FortuneCookie fc = new FortuneCookie();
+		fc.showButton();
 
-private void showButton() {
-	System.out.println("Button clicked");
 	}
-	
-}
 
+	private void showButton() {
+		JFrame bot = new JFrame();
+		bot.setVisible(true);
+		JButton button = new JButton();
+		bot.add(button);
+
+		System.out.println("Button clicked");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		System.out.println("Button clicked");
+
+		JOptionPane.showMessageDialog(null, "woohoo");
+		int rand = new Random().nextInt(5);
+		if (rand == 0) {
+			JOptionPane.showMessageDialog(null, "jack.jpeg");
+		} else if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "morgan.jpg");
+		} else if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "leonardo.jpeg");
+		} else if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "arnold.jpeg");
+		} else if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "jackinTheBox.png");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/JackInTheBox.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class JackInTheBox {
+	public static void main(String[] args) {
+		JFrame bot = new JFrame();
+		bot.setVisible(true);
+		bot.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
