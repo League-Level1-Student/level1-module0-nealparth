efa16afdfaf33e3091474301f63df5c90a64bf7d
MairaCCB <league@WTS14-4.attlocal.net>
2018-08-06 18:25:13 -0700

    FortuneCookie,JackInTheBox,SoundEffectMachine(IP)

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,6 +1,5 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,28 +17,14 @@ public class FortuneCookie implements ActionListener {
 		bot.setVisible(true);
 		JButton button = new JButton();
 		bot.add(button);
+		button.addActionListener(this);
 
-		System.out.println("Button clicked");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		System.out.println("Button clicked");
-
 		JOptionPane.showMessageDialog(null, "woohoo");
-		int rand = new Random().nextInt(5);
-		if (rand == 0) {
-			JOptionPane.showMessageDialog(null, "jack.jpeg");
-		} else if (rand == 1) {
-			JOptionPane.showMessageDialog(null, "morgan.jpg");
-		} else if (rand == 2) {
-			JOptionPane.showMessageDialog(null, "leonardo.jpeg");
-		} else if (rand == 3) {
-			JOptionPane.showMessageDialog(null, "arnold.jpeg");
-		} else if (rand == 4) {
-			JOptionPane.showMessageDialog(null, "jackinTheBox.png");
-		}
+
 		// TODO Auto-generated method stub
 
 	}
--- src/JackInTheBox.java
+++ src/JackInTheBox.java
@@ -1,10 +1,44 @@
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class JackInTheBox {
 	public static void main(String[] args) {
-		JFrame bot = new JFrame();
-		bot.setVisible(true);
-		bot.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+			JButton button = new JButton();
+			frame.add(button);
+			button.add(button, "jackInTheBox.png");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			java.net.URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
 	}
 }
