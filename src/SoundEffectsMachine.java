c4f3cb813dea4c42abb6f4de43f42f7a891165ac
nealparth <league@WorthyMini-5.attlocal.net>
2018-08-20 18:24:53 -0700

    SoundEffectsMachine,Cuteness TV(IP)

--- src/SoundEffectsMachine.java
+++ src/SoundEffectsMachine.java
@@ -1,4 +1,47 @@
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SoundEffectsMachine {
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
+public class SoundEffectsMachine implements ActionListener {
+public static void main(String[] args) {
+	SoundEffectsMachine em= new SoundEffectsMachine();
+	em.showButton();
 }
+
+public void showButton() {
+    System.out.println("Button clicked");
+    JFrame frame= new JFrame();
+    frame.setVisible(true);
+    JButton button= new JButton("sound1");
+    frame.add(button);
+    button.addActionListener(this);
+    JPanel panel= new JPanel();
+    frame.add(panel);
+    panel.add(button);
+    JOptionPane.showMessageDialog(null, "homer-woohoo.wav");
+    JButton button1= new JButton("sound2");
+    panel.add(button1);
+    button1.addActionListener(this);
+   
+
+  }
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+private void playSound(String fileName) {
+	
+    AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
+    sound.play();
+}
+}
+
